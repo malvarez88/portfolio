db675a17678c81fa751c543a9bca6d03a2464c60
Malvarez88 <marianoalvarez66@gmail.com>
2022-08-10 18:38:08 -0300

    contact section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import './footer.css'
+import React from "react";
+import "./footer.css";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <a href="#" className="footer__logo"><h3>Mariano Alvarez</h3></a>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+      </ul>
+      <div className="footer__copyright">
+        <small>&copy; Mariano Alvarez - All rights reserved - 2022</small>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
