b8496d4b97d9f7046a7245a28e1264a2440fe91d
Malvarez88 <marianoalvarez66@gmail.com>
2022-08-11 14:34:48 -0300

    about changes and cta in header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import "./header.css";
 import CTA from "./CTA";
-import logo from '../../assets/logoma.png'
+import logo from "../../assets/logoma.png";
 import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello I'm</h5>
-        <h1>Mariano Alvarez</h1>
-        <h5 className="text-ligth">Fullstack Developer</h5>
+        <h4>Hello I'm</h4>
+        <h1 class="glitch">Mariano Alvarez</h1>
+
+        <h3 className="text-ligth">Fullstack Developer</h3>
         <CTA />
         <HeaderSocials />
         <div className="me">
           <img src={logo} alt="logo" />
         </div>
-        <a href='#contact' className="scroll__down">Scroll Down</a>
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
       </div>
     </header>
   );
