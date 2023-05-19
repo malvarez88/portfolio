411752c8b76171c1dcb7091deb1df23fa23742f6
mariano alvarez <mariano@codingco.io>
2023-05-19 17:19:20 -0300

    color picker

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./footer.css";
-import {BsLinkedin} from 'react-icons/bs'
-import {BsGithub} from 'react-icons/bs'
+import { BsLinkedin } from "react-icons/bs";
+import { BsGithub } from "react-icons/bs";
 
-const Footer = () => {
+const Footer = ({ setTheme }) => {
+  const [selectedButton, setSelectedButton] = useState("regular");
+
+  const handleTheme = (e) => {
+    const selectedTheme = e.target.value;
+    e.preventDefault();
+    setTheme(selectedTheme);
+    setSelectedButton(selectedTheme);
+  };
   return (
     <footer>
-      <a href="#" className="footer__logo"><h3>Mariano Alvarez</h3></a>
+      <a href="#" className="footer__logo">
+        <h3>Mariano Alvarez</h3>
+      </a>
       <div className="footer__links">
-      <a href="https://www.linkedin.com/in/malvarez88/" rel="noreferrer" target="_blank"><BsLinkedin  className="footer__links-icon"/><br/> Linkedin</a>
-      <a href="https://github.com/malvarez88" rel="noreferrer" target="_blank"><BsGithub className="footer__links-icon"/> <br/> Github</a>
-    </div>
+        <a
+          href="https://www.linkedin.com/in/malvarez88/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <BsLinkedin className="footer__links-icon" />
+          <br /> Linkedin
+        </a>
+        <a
+          href="https://github.com/malvarez88"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <BsGithub className="footer__links-icon" /> <br /> Github
+        </a>
+      </div>
       <ul className="permalinks">
         <li>
           <a href="#">Home</a>
@@ -28,7 +51,51 @@ const Footer = () => {
           <a href="#contact">Contact</a>
         </li>
       </ul>
-  
+
+      <h3>Color Picker</h3>
+      <div className="theme-switcher">
+        <button
+          className="btn-switch"
+          aria-pressed={selectedButton === "regular"}
+          value="regular"
+          onClick={handleTheme}
+        >
+          Regular
+        </button>
+        <button
+          className="btn-switch"
+          aria-pressed={selectedButton === "violet"}
+          value="violet"
+          onClick={handleTheme}
+        >
+          Violet
+        </button>
+        <button
+          className="btn-switch"
+          aria-pressed={selectedButton === "red"}
+          value="red"
+          onClick={handleTheme}
+        >
+          Red
+        </button>
+        <button
+          className="btn-switch"
+          aria-pressed={selectedButton === "pink"}
+          value="pink"
+          onClick={handleTheme}
+        >
+          Pink
+        </button>
+        <button
+          className="btn-switch"
+          aria-pressed={selectedButton === "shine"}
+          value="shine"
+          onClick={handleTheme}
+        >
+          Shine
+        </button>
+      </div>
+
       <div className="footer__copyright">
         <small>&copy; Mariano Alvarez - All rights reserved - 2022</small>
       </div>
