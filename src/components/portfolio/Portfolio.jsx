1f92e484107baa6a6a038bf19ead73315a771e93
mariano alvarez <mariano@codingco.io>
2023-05-22 15:50:47 -0300

    /

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,37 +1,41 @@
+
 import React, { useEffect, useRef } from "react";
 import "./portfolio.css";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Slider from "../slider/Slider";
+import { projects } from "../../utils/constants";
 
 const Portfolio = () => {
-  const portfolioRef = useRef(null);
-  const headingRef = useRef(null)
+  const headingRef = useRef(null);
+  const portfolioSectionRef = useRef(null);
 
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
 
-    gsap.fromTo(
-      headingRef.current,
-      { opacity: 0 },
-      { opacity: 1, duration: 1, scrollTrigger: { trigger: headingRef.current, start: "top 80%" } }
-    );
+    gsap.to(portfolioSectionRef.current, {
+      position:'fixed',
+      width: "100%",
+      top: 0,
+      scrollTrigger: {
+        trigger: portfolioSectionRef.current,
+        start: "top top",
+        end: "+=100%",
+        pin: true,
+        pinSpacing: false,
+      },
+    });
 
-
+    ScrollTrigger.refresh();
   }, []);
 
-  useEffect(() => {
-    ScrollTrigger.refresh();
-  });
-
   return (
-    <section id="portfolio">
+    <section id="portfolio" ref={portfolioSectionRef}>
       <h5 ref={headingRef}>My Recent Work</h5>
       <h2 ref={headingRef}>Portfolio</h2>
-      <Slider ref={portfolioRef}/>
+      <Slider projects={projects} />
     </section>
   );
 };
 
 export default Portfolio;
-
--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,64 +1,70 @@
 import React, { useEffect, useRef } from "react";
-import gsap from "gsap";
+import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import "./slider.css";
-import { projects } from "../../utils/constants";
 
-const Slider = () => {
+const Slider = ({ projects }) => {
   const sliderRef = useRef(null);
+  console.log("🚀 ~ file: Slider.jsx:8 ~ Slider ~ sliderRef:", sliderRef)
   const projectsRef = useRef([]);
 
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
 
-    gsap.from(projectsRef.current, {
-      xPercent: -100,
-      opacity: 0,
-      duration: 1,
-      stagger: 0.2,
+    const tl = gsap.timeline({
       scrollTrigger: {
         trigger: sliderRef.current,
         start: "top center",
-        end: "bottom center",
+        end: () => `+=${sliderRef.current.offsetWidth}`,
         scrub: true,
       },
+    });
+
+    projectsRef.current.forEach((project) => {
+      tl.from(project, {
+        xPercent: 100,
+        opacity: 0,
+        duration: 1,
+        stagger: 0.2,
+        scrollTrigger: {
+          trigger: project,
+        },
+      });
     });
   }, []);
 
   return (
-    <section>
-      <div className="container slider__container" ref={sliderRef}>
-        {projects.map((project, index) => (
-          <div
-            className="project__container"
-            key={index}
-            ref={(el) => (projectsRef.current[index] = el)}
-          >
-            <h1 className="project__title">{project.slug}</h1>
-            <img
-              src={project.image}
-              alt={project.title}
-              className="project__image"
-            />
-            <div className="project__description">
-              <p>{project.description}</p>
-            </div>
-            <div className="portfolio__item-cta">
-              {project.cta ? (
-                <a
-                  href={project.github ? project.github : project.deploy}
-                  className="btn btn-primary"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  {project.cta}
-                </a>
-              ) : null}
-            </div>
+    <div className="container slider__container" ref={sliderRef}>
+      {projects.map((project, index) => (
+        <div
+          className="project__container"
+          key={index}
+          ref={(el) => (projectsRef.current[index] = el)}
+        >
+          <h1 className="project__title">{project.slug}</h1>
+          <img
+            src={project.image}
+            alt={project.title}
+            className="project__image"
+          />
+          <div className="project__description">
+            <p>{project.description}</p>
           </div>
-        ))}
-      </div>
-    </section>
+          <div className="portfolio__item-cta">
+            {project.cta ? (
+              <a
+                href={project.github ? project.github : project.deploy}
+                className="btn btn-primary"
+                target="_blank"
+                rel="noreferrer"
+              >
+                {project.cta}
+              </a>
+            ) : null}
+          </div>
+        </div>
+      ))}
+    </div>
   );
 };
 
